d545cbeef0d2e8f096601f973bfa51ad8d7a6461
i-smuglov <ysmuglov@gmail.com>
2021-07-08 14:13:57 +0300

    init

---
+++ day-lt.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Day.Lt scraper
+ * Plugin URI: https://github.com/i-smuglov/day-lt
+ * Description: This is the test task for scraping.
+ * Version: 1.0
+ * Author: Yuri Smuglov
+ * Author URI: https://github.com/i-smuglov/
+ **/
