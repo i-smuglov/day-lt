7bbdb55f7b017f72467d2e93e37bc342b74cacc3
i-smuglov <ysmuglov@gmail.com>
2021-07-09 17:57:56 +0300

    Remove staging code

--- scraper-functions.php
+++ scraper-functions.php
@@ -2,21 +2,7 @@
 // Get all website content
 function get_contents($url)
 {
-
-    if ($_SERVER['SERVER_NAME'] == "test") {
-        // Just for a local MAMP, no need to add this on a production server (SSL error)
-        $arrContextOptions = array(
-            "ssl" => array(
-                "verify_peer" => false,
-                "verify_peer_name" => false,
-            ),
-        );
-        $content = file_get_contents($url, false, stream_context_create($arrContextOptions));
-
-    } else {
-        // For a production server
-        $content = file_get_contents($url);
-    }
+    $content = file_get_contents($url);
 
     // Deal with windows-1257 charset
     return mb_convert_encoding($content, "UTF-8", "HTML-ENTITIES");
@@ -48,11 +34,11 @@ function transform_day_names($day_names_arr)
 // TODO: get date of scraping from a website instead of a local one
 function save_day_names($day_names_list)
 {
-   /*
-   * Saving data do a database with update_option() works 'like' a cache here, because we don't need to scrape it every time now.
-   * From that point any kind of a database cache plugin will work just fine, but we also could just use wp_cache_***() instead.
-   * Also, ***_transient() is theoretically better in this use-case, but requires more QA.
-   */
+    /*
+    * Saving data do a database with update_option() works 'like' a cache here, because we don't need to scrape it every time now.
+    * From that point any kind of a database cache plugin will work just fine, but we also could just use wp_cache_***() instead.
+    * Also, ***_transient() is theoretically better in this use-case, but requires more QA.
+    */
     update_option('day_names', array($day_names_list, date("m.d.y"))); // [0] - data, [1] - timestamp
 }
 
